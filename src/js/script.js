bf34c13e945f463c642ddcbb9b75c61ae84f0700
adrianojrvidal <adriano.jv@hotmail.comm>
2026-04-22 17:54:35 -0300

    Section Products e Footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// Botão Header
+// Botão Menu Mobile do Header
 
 const mobileBtn = document.querySelector('.btn-mobile');
 const navLinks = document.querySelector('#nav-links');
@@ -9,4 +9,16 @@ mobileBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-bars');
     icon.classList.toggle('fa-times');
 });
+
+
+// Efeito de Box Shadow no Header a partir do scroll
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('header');
+
+    if (window.scrollY > 0) {
+        header.style.boxShadow = '0 4px 10px rgba(0, 0, 0, 0.1)';
+    } else {
+        header.style.boxShadow = 'none';
+    }
+})
 
